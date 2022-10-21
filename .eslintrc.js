8b854a90a4f02f99787eb4a9f430071b44002771
Zohair Osama <zohairosama@192.168.1.5>
2022-10-21 17:27:59 +0200

    existing file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
     'env': {
         'browser': true,
@@ -25,13 +26,5 @@ module.exports = {
             'error',
             'unix'
         ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
     }
 }
